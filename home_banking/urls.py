8c1d94708fe6c970c020cc40ebc844256fdcc20b
Karen Fischer <kfischer733@gmail.com>
2025-10-17 05:31:28 -0300

    Se agregan endpoints

--- home_banking/urls.py
+++ home_banking/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='login', permanent=False), name='home'),
     path('admin/', admin.site.urls),
     path('cuenta/', include('cuenta.urls')),
     path('transferencia/', include('transferencia.urls')),
